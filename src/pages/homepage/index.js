f8d34cf3d073d9bad3abf40d35f1e8d2d5daf16e
Douglas Hammarstam <dogge20@hotmail.com>
2020-10-08 12:36:03 +0200

    test

--- src/pages/homepage/index.js
+++ src/pages/homepage/index.js
@@ -6,34 +6,12 @@ const TEST_URL = "http://www.nasa.gov/rss/dyn/breaking_news.rss"
 
 const HomePage = (props) => {
 
-    props.rssfeed.map((item) => {
-        console.log(item)
-    })
 
     return(
         <div className="homepage">
-<<<<<<< HEAD
             <ul>
-                {props.rssfeed.map((item) => {
-                    return(<li>{item}</li>)
-                })}
+
             </ul>
-=======
-            <h1>* Norska skidskyttar trotsar corona..303
-                                        
-                                        * Skador stör förtegen Djokovic......304
-                                                                                
-                                        * Coe vill tillåta knäböjning i OS...305
-                                                                                
-                                        * Rammad Giro-cyklist utskriven......306
-                                                                                
-                                        * Skidsporten hotas av Vasa-smäll....307
-                                        * NOTISER: Nordenkampen blir av......328
-                                                                                
-                                          Sport onsdag:                         
-                                          Slog 15 år gammalt världsrekord....308
-                                          ...världsrekord även på 5000 m.....309</h1>
->>>>>>> master
         </div>
     )
 }
