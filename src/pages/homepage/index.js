e9d2572df180a92f232b3136273f60c4ee48f0c9
Kamal <kamal.al-wendawi@afaforsakring.se>
2020-10-08 12:19:05 +0200

    Drawer added

--- src/pages/homepage/index.js
+++ src/pages/homepage/index.js
@@ -4,7 +4,20 @@ import React from 'react';
 const HomePage = () => {
     return(
         <div className="homepage">
-            <h1>Homepage</h1>
+            <h1>* Norska skidskyttar trotsar corona..303
+                                        
+                                        * Skador stör förtegen Djokovic......304
+                                                                                
+                                        * Coe vill tillåta knäböjning i OS...305
+                                                                                
+                                        * Rammad Giro-cyklist utskriven......306
+                                                                                
+                                        * Skidsporten hotas av Vasa-smäll....307
+                                        * NOTISER: Nordenkampen blir av......328
+                                                                                
+                                          Sport onsdag:                         
+                                          Slog 15 år gammalt världsrekord....308
+                                          ...världsrekord även på 5000 m.....309</h1>
         </div>
     )
 }
--- src/pages/submitpage/index.js
+++ src/pages/submitpage/index.js
@@ -4,7 +4,10 @@ import React from 'react';
 const SubmitPage = () => {
     return(
         <div className="submitpage">
-            <h1>submitpage</h1>
+            <h1>Använd text, inte bilder, för att visa text
+Användare behöver då och då anpassa texten bland annat genom att förstora eller välja ett annat teckensnitt, ändra förgrund- och bakgrundsfärger eller linjeavstånd. Om texten utgör en del av en bild saknas ofta de möjligheterna.
+
+Många användare behöver anpassa texten bland annat genom att förstora eller välja ett annat teckensnitt, ändra förgrund- och bakgrundsfärger eller linjeavstånd. Som regel har användare goda möjligheter att kontrollera hur digital text presenteras. Men när texten utgör en del av en bild saknas ofta de möjligheterna. Då blir bilden och texten pixlig om den förstoras och det är svårt eller omöjligt att byta teckensnitt eller färger. Använd därför så långt det är möjligt text för att presentera information i stället för text i form av en bild.</h1>
         </div>
     )
 }
