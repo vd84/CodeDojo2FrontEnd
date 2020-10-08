fbdfbb319dad64241a499b541ffddf94d2270629
Douglas Hammarstam <dogge20@hotmail.com>
2020-10-08 14:05:02 +0200

    list of rssItems homepage

--- src/pages/homepage/index.js
+++ src/pages/homepage/index.js
@@ -1,17 +1,15 @@
 import React from "react";
 
-
-
 const HomePage = (props) => {
-
-
-    return(
-        <div className="homepage">
-            <ul>
-
-            </ul>
-        </div>
-    )
-}
+  return (
+    <div className="homepage">
+      <ul>
+        {props.rssData.map((rssItem) => {
+          return <li key={rssItem.entryTitle}>{rssItem.entryTitle}</li>;
+        })}
+      </ul>
+    </div>
+  );
+};
 
 export default HomePage;
